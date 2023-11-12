a888d26300ac29c3d5196fab78805e128496f144
Tomy Romero <tomyfletcher99@hotmail.com>
2023-11-12 19:55:21 -0400

    database continued testing

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,27 +1,35 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react';
 import { currentUser } from '@clerk/nextjs';
+import { fetchUser } from '@/lib/actions/user.actions';
 
-const page = () => {
+const ProfilePage = () => {
+  const [userData, setUserData] = useState(null);
 
-  console.log("hello from vercel")
-  const getUser = async () => {
-    console.log("TEST")
+  const load = async () => {
+    try {
+      const userId = 'user_2XSaXcIXd9dXAjxVuNhq0L7RXkk';
+      const userInfo = await fetchUser(userId);
+      setUserData(userInfo);
+    } catch (error) {
+      console.error('Error loading user data:', error);
+    }
+  };
 
-    const user = await currentUser();
-
-    console.log("USER: ", user?.id)
-
-    return user?.id;
-  }
-  
-  const data = getUser();
+  useEffect(() => {
+    load();
+  }, []); // Empty dependency array ensures the effect runs once on mount
 
   return (
     <div>
       <h1>Profile!!</h1>
-      <h2>{data}</h2>
+      {userData ? <h2>{
+        //@ts-ignore
+      userData.username
+      }</h2> : <p>Loading...</p>}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default ProfilePage;
