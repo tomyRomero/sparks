3c6c1717057df62ddee0c9748d25369405fe60fd
Tomy Romero <tomyfletcher99@hotmail.com>
2023-11-12 18:47:37 -0400

    test

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { currentUser } from '@clerk/nextjs';
 
 const page = () => {
+
+  const getUser = async () => {
+    console.log("TEST")
+
+    const user = await currentUser();
+    
+    console.log("USER: ", user)
+
+    
+  }
+  
+
   return (
     <div>Profile</div>
   )
