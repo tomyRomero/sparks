10ac0c7a26b31458f711a029722bd38fab64194d
Tomy Romero <tomyfletcher99@hotmail.com>
2023-11-12 17:25:30 -0400

    page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,55 +1,9 @@
-import Post from "@/components/cards/Post";
-import UserImg from "@/components/shared/UserImg";
-import { fetchPosts} from "@/lib/actions/post.actions";
-import { currentUser } from "@clerk/nextjs";
-import { fetchUser } from "@/lib/actions/user.actions";
-import { redirect, useRouter } from "next/navigation";
+import React from 'react'
 
-async function Home({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | undefined };
-}) {
-  const user = await currentUser();
-  if (!user) return null;
-
-  const userInfo = await fetchUser(user.id);
-  if (!userInfo?.onboarded) redirect("/onboarding");
-
-  const result: any = await fetchPosts(
-    searchParams.page ? +searchParams.page : 1,
-     30, 
-  )  
-
- return (
-    <>
-      <h1 className='head-text text-left text-black'>Home</h1>
-      <section className='mt-9 flex flex-col gap-10'>
-        {result?.length === 0 ? (
-          <p className='no-result'>No posts found</p>
-        ) : (
-          <>
-          
-            {result.map((post: any) => (
-              <Post
-                key={post.idpost}
-                id={post.idpost}
-                currentUserId={user.id}
-                parentId={post.parent_Id}
-                content={post.content}
-                createdAt={post.created_at}
-                comments={post.children}
-                image={post.author_image}
-                username={post.author_username}
-                likes = {post.likes? post.likes: ''}
-              />
-            ))}
-
-          </>
-        )}
-      </section>
-    </>
-  );
+const page = () => {
+  return (
+    <div>page</div>
+  )
 }
 
-export default Home;
+export default page
